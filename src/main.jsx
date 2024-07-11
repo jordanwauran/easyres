8cd9b1da561645e0ea5045e4d657d892b67fcedc
jordanwauran <jordanwauran@gmail.com>
2024-07-11 13:18:52 -0700

    second deployment

--- src/main.jsx
+++ src/main.jsx
@@ -44,7 +44,7 @@ const router=createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={process.env.VITE_CLERK_PUBLISHABLE_KEY}>
       <RouterProvider router={router}/>
     </ClerkProvider>
   </React.StrictMode>,
